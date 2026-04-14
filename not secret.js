c02f77f75eb239dc94075a7f5db3f64e3c82061d
Heidi Galvan <hgalvan2025@fau.edu>
2026-04-14 03:04:44 +0000

    Fix OPENAI_API_URL constant definition

--- not secret.js
+++ not secret.js
@@ -1,1 +1,3 @@
-const API_KEY ="sk-proj-pW0YtrpAqLSgmabKmohZAOoDVYV16eGnzL2NkDFrky4pp2IMKOWvSsW72KCo0Fwq6SFKU-jmncT3BlbkFJliDq-qoZkdcj5wH935hkGPouDAZvtydDBreipHnsJApgNbV_0LDCFDGuXuRPUN-IFbKa19GYoAE";
+const OPENAI_API_KEY =
+  "sk-proj-pW0YtrpAqLSgmabKmohZAOoDVYV16eGnzL2NkDFrky4pp2IMKOWvSsW72KCo0Fwq6SFKU-jmncT3BlbkFJliDq-qoZkdcj5wH935hkGPouDAZvtydDBreipHnsJApgNbV_0LDCFDGuXuRPUN-IFbKa19GYoAE";
+const OPENAI_API_URL = "https://api.openai.com/v1/chat/completions";
