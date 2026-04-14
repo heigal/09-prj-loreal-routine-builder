f3100cef09d6ec9816d0b5ee4a2ad68c6291786f
Heidi Galvan <hgalvan2025@fau.edu>
2026-04-14 02:24:24 +0000

    Update local OpenAI config file

--- not secret.js
+++ not secret.js
@@ -1,3 +1,1 @@
-const OPENAI_API_KEY =
-  "PASTE_YOUR_OPENAI_API_KEY_HERE";
-const OPENAI_API_URL = "https://api.openai.com/v1/chat/completions";
+const API_KEY ="sk-proj-pW0YtrpAqLSgmabKmohZAOoDVYV16eGnzL2NkDFrky4pp2IMKOWvSsW72KCo0Fwq6SFKU-jmncT3BlbkFJliDq-qoZkdcj5wH935hkGPouDAZvtydDBreipHnsJApgNbV_0LDCFDGuXuRPUN-IFbKa19GYoAE";
